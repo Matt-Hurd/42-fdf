24dee136ae484551830490d96cc1b98b3d36cf1f
Matt-Hurd <sabahbob@gmail.com>
2016-10-18 14:20:52 -0700

    ayy

---
+++ includes/fdf.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhurd <mhurd@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/08 16:06:16 by mhurd             #+#    #+#             */
+/*   Updated: 2016/10/18 11:36:02 by mhurd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+# include "mlx.h"
+# include "libft.h"
+# include "libgfx.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <errno.h>
+
+/*
+** KEYS
+*/
+
+# define KEY_ESC 53
+
+# define KEY_UP 126
+# define KEY_DOWN 125
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+
+# define KEY_ZOOM_IN 69
+# define KEY_ZOOM_OUT 78
+
+# define KEY_ROT_X_U 86
+# define KEY_ROT_X_D 83
+# define KEY_ROT_Y_U 87
+# define KEY_ROT_Y_D 84
+# define KEY_ROT_Z_U 88
+# define KEY_ROT_Z_D 85
+
+t_plot		*parse_file(char *filename);
+void		draw_everything(t_data *d);
+int			key_hook(int keycode, t_data *d);
+void		draw_reload(t_data *d);
+void		ft_exit(void);
+void		ft_error(char *s);
+void		ft_error_unknown(void);
+int			expose_hook(t_data *d);
+#endif
---
+++ libgfx/libgfx.h
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libgfx.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhurd <mhurd@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/08 16:42:11 by mhurd             #+#    #+#             */
+/*   Updated: 2016/10/18 11:34:53 by mhurd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBGFX_H
+# define LIBGFX_H
+# include "mlx.h"
+# include "libft.h"
+# include <stdlib.h>
+# include <math.h>
+# define ABS(x) ((x > 0) ? x : x * -1)
+# define COLOR2 0x16A085
+# define COLOR1 0xF4D03F
+# define WINDOW_SIZE_X 1000
+# define WINDOW_SIZE_Y 700
+
+typedef struct	s_2d
+{
+	short	x;
+	short	y;
+}				t_2d;
+
+typedef struct	s_3d
+{
+	float	x;
+	float	y;
+	float	z;
+}				t_3d;
+
+typedef struct	s_vertex
+{
+	t_3d	*local;
+	t_3d	*world;
+	t_3d	*aligned;
+}				t_vertex;
+
+typedef struct	s_plot
+{
+	t_vertex	***points;
+	int			width;
+	int			height;
+	float		z_min;
+	float		z_max;
+}				t_plot;
+
+typedef struct	s_data
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*pixel_img;
+	float	xr;
+	float	yr;
+	float	zr;
+	float	scale;
+	float	xtrans;
+	float	ytrans;
+	int		bpp;
+	int		s_line;
+	int		ed;
+	t_plot	*plot;
+}				t_data;
+
+void			ft_mat_copy(float source[4][4], float dest[4][4]);
+void			ft_mat_mult(float m1[4][4], float m2[4][4], float d[4][4]);
+void			ft_vec_mult_mat(t_3d *s, float mat[4][4], t_3d *d);
+float			ft_cos(int x);
+float			ft_sin(int x);
+void			ft_tr_translate(float m[4][4], float tx, float ty, float tz);
+void			ft_tr_scale(float matrix[4][4], float sx, float sy, float sz);
+void			ft_tr_rotate(float matrix[4][4], float ax, float ay, float az);
+void			ft_make_identity_matrix(float matrix[4][4]);
+void			ft_3d_drawline(t_data *d, t_3d p1, t_3d p2);
+t_3d			*ft_make_3d(int x, int y, int z);
+t_vertex		*ft_make_vertex(int x, int y, int z);
+#endif
